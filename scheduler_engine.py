f4dd02b29101b2ad12f1ff65bf8f18df7f975523
abd4llh <40068138+abd4llh@users.noreply.github.com>
2026-06-08 20:27:35 +0200

    Restore and implement v0.6 scoring scheduler

--- scheduler_engine.py
+++ scheduler_engine.py
@@ -7,4 +7,4 @@ from parser_utils import hhmm_to_minutes, minutes_to_hhmm
 class Scheduler:
     def __init__(self, wake_min=360, sleep_min=1380, slot_min=15, protect_weekend=True, planning_mode="Balanced week"):
         self.wake_min, self.sleep_min, self.slot_min = wake_min, sleep_min, slot_min
-        self.protect_weekend, self.planning_mode = protect_weekend, planning_mode
+        self.protect_weekend,
